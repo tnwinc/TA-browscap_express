bcce231733e4984e3635b0f43561cb7c5d966ac1
Robert Labrie <robert.labrie@gmail.com>
2014-08-10 14:00:09 -0400

    read datafiles before processing STDIN

--- bin/browscap_lookup.py
+++ bin/browscap_lookup.py
@@ -18,54 +18,51 @@ def data_to_dict(headers, data):
 	return out
 	
 #the red meat
-def browser_lookup(datafile,http_user_agent):	
-	with open(datafile, 'r') as f:
-		junk = f.readline()	#first line is junk
-		junk = f.readline() #second line is junk
-		header = f.readline() #third line is headers
-		defaults = f.readline() #fourth line is defaults
+def browser_lookup(data,http_user_agent):	
+	header = data[2]
+	defaults = data[3]
 
-		#repackage the header into something useful
-		csv_reader = csv.reader( [ header ] )
-		for row in csv_reader:
-			header = row
-		
-		#repackage the defaults into something useful
-		csv_reader = csv.reader( [ defaults ] )
-		for row in csv_reader:
-			defaults = row
+	#repackage the header into something useful
+	csv_reader = csv.reader( [ header ] )
+	for row in csv_reader:
+		header = row
+	
+	#repackage the defaults into something useful
+	csv_reader = csv.reader( [ defaults ] )
+	for row in csv_reader:
+		defaults = row
 
-		#initialize a blank out object with default files
-		out = dict()
-		out['browser_data_raw'] = defaults
-		out['browser_data'] = data_to_dict(header,defaults)
-		
-		#for short, or unspecified UA strings, just return the default
-		if len(http_user_agent) < 2:
-			return out
-
-		#best_regex tracks the regex which will eventually be used for a match
-		best_regex = ''
-
-		#with the headers handled, start to enumerate data
-		for line in f:
-			csv_reader = csv.reader( [ line ] )
-			for row in csv_reader:
-				bits = row
-			
-			#convert the browscap string to a regex string. The length
-			#comparison solves the problem of finding the "best" match. In the
-			#ini file, the best matches come first, but the CSV is a mess.
-			#In this case, enumerate the entire file, and the longest regex
-			#string will have the most detail and therefore is the best match
-			#This logic lifted without shame from pybrowscap
-			ua_regex = '^{0}$'.format(re.escape(bits[0]))
-			ua_regex = ua_regex.replace('\\?', '.').replace('\\*', '.*?')
-			if (re.search(ua_regex,http_user_agent)) and len(ua_regex) > len(best_regex):
-				best_regex = ua_regex
-				out['browser_data_raw'] = line
-				out['browser_data'] = data_to_dict(header,bits)
+	#initialize a blank out object with default files
+	out = dict()
+	out['browser_data_raw'] = defaults
+	out['browser_data'] = data_to_dict(header,defaults)
+	
+	#for short, or unspecified UA strings, just return the default
+	if len(http_user_agent) < 2:
 		return out
+
+	#best_regex tracks the regex which will eventually be used for a match
+	best_regex = ''
+
+	#with the headers handled, start to enumerate data
+	for line in data[3:]:
+		csv_reader = csv.reader( [ line ] )
+		for row in csv_reader:
+			bits = row
+		
+		#convert the browscap string to a regex string. The length
+		#comparison solves the problem of finding the "best" match. In the
+		#ini file, the best matches come first, but the CSV is a mess.
+		#In this case, enumerate the entire file, and the longest regex
+		#string will have the most detail and therefore is the best match
+		#This logic lifted without shame from pybrowscap
+		ua_regex = '^{0}$'.format(re.escape(bits[0]))
+		ua_regex = ua_regex.replace('\\?', '.').replace('\\*', '.*?')
+		if (re.search(ua_regex,http_user_agent)) and len(ua_regex) > len(best_regex):
+			best_regex = ua_regex
+			out['browser_data_raw'] = line
+			out['browser_data'] = data_to_dict(header,bits)
+	return out
 
 #is_known_browser solves the problem of not recording Generic or Default
 #browsers. Since the cache file is always checked first, if a generic was
@@ -85,13 +82,18 @@ def is_known_browser(browser_data):
 #
 if __name__ == '__main__':
 
-	# init the browscap class
+	uacache = dict()
+	scriptpath = os.path.dirname(os.path.realpath(__file__))
+	
+	#read the databases into memory
+	browscapdata_lite = open(scriptpath + '\\browscap_lite.csv').readlines()
+	browscapdata = open(scriptpath + '\\browscap.csv').readlines()
+
 	r = csv.reader(sys.stdin)
 	w = csv.writer(sys.stdout)
 	have_header = False
 	header = []
 	idx = -1
-	uacache = dict()
 	for row in r:
 		if (have_header == False):
 			header = row
@@ -113,16 +115,18 @@ if __name__ == '__main__':
 			results = uacache[http_user_agent]
 		else:
 			#print "check the cache"
-			browser_data = browser_lookup(os.path.dirname(os.path.realpath(__file__)) + '\\browscap_lite.csv',http_user_agent)
+			browser_data = browser_lookup(browscapdata_lite,http_user_agent)
 			browser_data['browser_data']['ua_fromcache'] = 'true'
 
 			#no mas? check the full dataset
 			if (browser_data['browser_data']['ua_browser'] == 'DefaultProperties'):
 				#print "checking master"
-				browser_data = browser_lookup(os.path.dirname(os.path.realpath(__file__)) + '\\browscap.csv',http_user_agent)
+				browser_data = browser_lookup(browscapdata,http_user_agent)
 				browser_data['browser_data']['ua_fromcache'] = 'false'
 				if (is_known_browser(browser_data['browser_data'])):
-					with open(os.path.dirname(os.path.realpath(__file__)) + '\\browscap_lite.csv','a') as browscap_file:
+					#write a match back to the currently cached browscapdata_lite and to it's backing file
+					browscapdata_lite.append(browser_data['browser_data_raw'])
+					with open(scriptpath + '\\browscap_lite.csv','a') as browscap_file:
 						browscap_file.write(browser_data['browser_data_raw'])
 			results = browser_data['browser_data']
 			uacache[http_user_agent] = results
